d9f4a9e50dadf1349ba176f9233a45f2719a24c3
Hayden Curran <haydenz@gmail.com>
2026-08-09 01:54:41 +0000

    test: add quest script test prerequisites

---
+++ Goose.Tests/Collections/GameWorldSettingsCollection.cs
@@ -0,0 +1,7 @@
+namespace Goose.Tests.Collections;
+
+[CollectionDefinition(Name, DisableParallelization = true)]
+public sealed class GameWorldSettingsCollection
+{
+    public const string Name = "GameWorld settings";
+}
