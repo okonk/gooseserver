28860ae30bd7dbc8b0c17d7d1ac50fed143b1ec8
Hayden Curran <haydenz@gmail.com>
2026-08-27 10:14:58 +0000

    fix: default missing GooseSettings string fields to empty (deferred null bug #19)

--- Goose/FatalStartupException.cs
+++ Goose/FatalStartupException.cs
@@ -3,6 +3,8 @@ namespace Goose
 {
     public sealed class FatalStartupException : Exception
     {
+        public FatalStartupException(string message) : base(message) { }
+
         public FatalStartupException(string message, Exception innerException)
             : base(message, innerException)
         {
